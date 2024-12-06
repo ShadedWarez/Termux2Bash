2a4273e54a904faf7a5b10d16dfcbac384f36040
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-12-06 15:35:12 +0800

    Update install.sh

--- KaliLinux/rcfiles/install.sh
+++ KaliLinux/rcfiles/install.sh
@@ -5,5 +5,5 @@ usermod -aG sudo kali
 echo "kali    ALL=(ALL:ALL) ALL" >>/etc/sudoers
 ###
 wget -O /root/.bashrc https://raw.githubusercontent.com/xiv3r/Termux-Pentesting-Distro/refs/heads/main/KaliLinux/rcfiles/rootbash.bashrc
-wget -O /home/.bashrc https://raw.githubusercontent.com/xiv3r/Termux-Pentesting-Distro/refs/heads/main/KaliLinux/rcfiles/homebash.bashrc
+wget -O /home/kali/.bashrc https://raw.githubusercontent.com/xiv3r/Termux-Pentesting-Distro/refs/heads/main/KaliLinux/rcfiles/homebash.bashrc
 wget -O /etc/bash.bashrc https://raw.githubusercontent.com/xiv3r/Termux-Pentesting-Distro/refs/heads/main/KaliLinux/rcfiles/etcbash.bashrc
