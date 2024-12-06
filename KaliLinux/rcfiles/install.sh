1009f180d5b9e32e091f10d7336363cc75651844
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-12-06 11:31:53 +0800

    Update install.sh

--- KaliLinux/rcfiles/install.sh
+++ KaliLinux/rcfiles/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-wget -O /root/.zshrc
-wget -O /home/.zshrc
-wget -O /etc/bash.bashrc
+wget -O /root/.bashrc https://raw.githubusercontent.com/xiv3r/Termux-Pentesting-Distro/refs/heads/main/KaliLinux/rcfiles/rootbash.bashrc
+wget -O /home/.bashrc https://raw.githubusercontent.com/xiv3r/Termux-Pentesting-Distro/refs/heads/main/KaliLinux/rcfiles/homebash.bashrc
+wget -O /etc/bash.bashrc https://raw.githubusercontent.com/xiv3r/Termux-Pentesting-Distro/refs/heads/main/KaliLinux/rcfiles/etcbash.bashrc
