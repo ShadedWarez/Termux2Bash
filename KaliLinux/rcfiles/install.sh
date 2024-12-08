3d5541eeae591711d70829c847686a334def53f7
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-12-08 11:53:55 +0800

    Delete KaliLinux/rcfiles/install.sh

--- KaliLinux/rcfiles/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-wget -O /root/.bashrc https://raw.githubusercontent.com/xiv3r/Termux-Pentesting-Distro/refs/heads/main/KaliLinux/rcfiles/rootbash.bashrc
-wget -O /home/kali/.bashrc https://raw.githubusercontent.com/xiv3r/Termux-Pentesting-Distro/refs/heads/main/KaliLinux/rcfiles/homebash.bashrc
-wget -O /etc/bash.bashrc https://raw.githubusercontent.com/xiv3r/Termux-Pentesting-Distro/refs/heads/main/KaliLinux/rcfiles/etcbash.bashrc
